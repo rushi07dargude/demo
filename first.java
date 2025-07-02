00bfaee2c5a8cee1be986152e2bc001a4ecba8a2
rushi07dargude <162776388+rushi07dargude@users.noreply.github.com>
2025-07-02 16:06:03 +0530

    Update first.java

--- first.java
+++ first.java
@@ -3,6 +3,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("This is java file");
         System.out.println("My name is Rushi Dargude");
-        System.out.println("I am Full Stack Developer");
+        System.out.println("I am Full Stack Developer")
     }
 }
