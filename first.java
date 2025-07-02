a8a9e6cad54694da5068c36dfc1bfad1278fed4a
rushi07dargude <162776388+rushi07dargude@users.noreply.github.com>
2025-07-02 15:37:00 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first {
+
+    public static void main(String[] args) {
+        System.out.println("This is java file");
+        System.out.println("My name is Rushi Dargude");
+    }
+}
